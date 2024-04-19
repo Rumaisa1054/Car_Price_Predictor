700fa6e82d2a71f4c416ddd4acca4a77b4aa87eb
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2024-04-20 02:39:40 +0500

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,53 +1,45 @@
 import streamlit as st
-from clarifai.client.auth import create_stub
-from clarifai.client.auth.helper import ClarifaiAuthHelper
-from clarifai.client.user import User
-from clarifai.modules.css import ClarifaiStreamlitCSS
-from google.protobuf import json_format, timestamp_pb2
+import pandas as pd
+import numpy as np
+import pickle
 
 st.set_page_config(layout="wide")
-ClarifaiStreamlitCSS.insert_default_css(st)
 
-# This must be within the display() function.
-auth = ClarifaiAuthHelper.from_streamlit(st)
-stub = create_stub(auth)
-userDataObject = auth.get_user_app_id_proto()
+def load_model():
+    with open('LinearRegressionModel.pkl', 'rb') as file:
+        model = pickle.load(file)
+    return model
 
-st.title("Simple example to list inputs")
+pipe = load_model()
 
-with st.form(key="data-inputs"):
-  mtotal = st.number_input(
-      "Select number of inputs to view in a table:", min_value=5, max_value=100)
-  submitted = st.form_submit_button('Submit')
+st.title("Car Price Prediction App")
 
-if submitted:
-  if mtotal is None or mtotal == 0:
-    st.warning("Number of inputs must be provided.")
-    st.stop()
-  else:
-    st.write("Number of inputs in table will be: {}".format(mtotal))
+def main():
+    with st.sidebar:
+        st.subheader("Enter Car Details")
+        df = pd.read_csv("Cleaned_Car_data.csv")
+        companies = df['company']
+        
+        years = df['year']
+        fuel_types = df['fuel_type']
+        kms_drivens = df['kms_driven']
+        company = st.selectbox("Company", companies)
+        # Filter rows where 'company' column equals 'company_name'
+        filtered_df = df[df['company'] == company]
+        # Extract the 'name' column from the filtered DataFrame
+        models = filtered_df['name'].tolist()
+        model = st.selectbox("Model", models)
+        year = st.selectbox("Year of Purchase",years)
+        fuel_type = st.selectbox("Fuel Type", fuel_types)
+        kms_driven = st.number_input("Approximate Number of Kilometers Driven", min_value=0,)
 
-  # Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
-  input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
-  all_inputs = input_obj.list_inputs()
+    if not company or not model or not year or not fuel_type or not kms_driven:
+        st.warning("Please enter all car details to continue")
 
-  #Check for no of inputs in the app and compare it with no of inputs to be displayed.
-  if len(all_inputs) < (mtotal):
-    raise Exception(
-        f"No of inputs is less than {mtotal}. Please add more inputs or reduce the inputs to be displayed !"
-    )
+    if st.sidebar.button("Submit"):
+        question = f"Car Details: {company}, {model}, Year: {year}, Fuel Type: {fuel_type}, KMs Driven: {kms_driven}"
+        prediction = pipe.predict(pd.DataFrame(columns=['name','company','year','kms_driven','fuel_type'],data=np.array([model, company, year, kms_driven, fuel_type]).reshape(1,5)))
+        st.write("Prediction:", prediction)
 
-  else:
-    data = []
-    #added "data_url" which gives the url of the input.
-    for inp in range(mtotal):
-      data.append({
-          "id": all_inputs[inp].id,
-          "data_url": all_inputs[inp].data.image.url,
-          "status": all_inputs[inp].status.description,
-          "created_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].created_at),
-          "modified_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].modified_at),
-          "metadata": json_format.MessageToDict(all_inputs[inp].data.metadata),
-      })
-
-  st.dataframe(data)
+if __name__ == '__main__':
+    main()
